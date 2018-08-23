58b960966c59f19336c28e31ef7d30ab587804b7
mengfan <illusionai@163.com>
2018-08-23 15:26:30 +0800

    新增文件

---
+++ bos_management/src/test/java/cn/itcast/bos/web/action/take_delivery/PromotionActionTest.java
@@ -0,0 +1,16 @@
+package cn.itcast.bos.web.action.take_delivery;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PromotionActionTest {
+
+
+    @Test
+    public void test(){
+        System.out.println("输出信息");
+        System.out.println("输出信息");
+        System.out.println("输出信息");
+    }
+}
